9542c234d1f73914fa96b84c283c16e074b4f88a
Thales <thalesteng@gmail.com>
2019-07-20 17:22:54 -0300

    Added more logic specific to dictionaries

--- dic.py
+++ dic.py
@@ -17,3 +17,18 @@ ages = [23, 18, 34, 56, 27]
 dic = {key:value for key, value in zip(names, ages)}
 
 print(dic)
+
+#get allows to return value from a specific key without worrying whether key exists or not, params are key and default return value
+#the same analogy goes on for a pop method
+
+print(dic.get("thales", 0))
+print(dic.pop("thalez", 99))
+
+list_keys = dic.keys()
+print(list_keys)
+
+list_vals = dic.values()
+print(list_vals)
+
+for n, a in dic.items():
+    print("{} is {} years old".format(n, a))
