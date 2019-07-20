c16f453892ef6548d0145cbae2fe0daa4f383f7b
Thales <thalesteng@gmail.com>
2019-07-20 09:06:37 -0300

    added basic list examples

---
+++ lists.py
@@ -0,0 +1,17 @@
+#zip: takes two or more lists and returns an object that contains a list of tuples
+
+names = ['thales', 'another thales', 'tylex', 'sales']
+
+ages = [23, 26, 19, 56]
+
+
+
+names_and_ages = zip(names, ages)
+
+print(list(names_and_ages))
+
+l = []
+
+l.append(1)
+
+print(l)
