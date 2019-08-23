76dfba301dd2e44b8b7956228544e779204d0de2
Thales <thalesteng@gmail.com>
2019-08-23 11:24:31 -0300

    Adding examples of vectorized computation, starting by basic arithmetic operations

---
+++ numpy/vectorized-computation/basic-arithmetic.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+A = np.array([[0.84, -0.26, 0.92], [0.42, 0.31, -0.73]])
+print("A array:")
+print(A)
+B = A*10
+print("B array (A*10):")
+print(B)
+C = A + A
+print("C array (A+A):")
+print(C)
